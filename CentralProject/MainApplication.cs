4c26e9e2ea4a9e7e00440b4793e9d7ae39b60f03
Alexander Savinoff <hinstance@yandex.ru>
2013-01-04 19:32:58 +0400

    u

---
+++ CentralProject/MainApplication.cs
@@ -0,0 +1,29 @@
+﻿#define DEBUG_WRITE
+
+using System;
+using System.Threading;
+using System.Globalization;
+
+namespace CentralProject
+{
+
+	class MainApplication
+	{
+
+		/// <summary>
+		/// Main entry point
+		/// </summary>
+		/// <param name="args"></param>
+		static void Main(string[] args)
+		{
+		}
+
+		public MainApplication(string[] args)
+		{
+			Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("en-us");
+			Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("en-us");
+		}
+
+	}
+
+}
