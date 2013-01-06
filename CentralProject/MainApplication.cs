b51081530f1073471f4d95aa5fe8d15c9a40c6a8
Alexander Savinoff <hinstance@yandex.ru>
2013-01-06 16:17:55 +0400

    update

--- CentralProject/MainApplication.cs
+++ CentralProject/MainApplication.cs
@@ -4,6 +4,10 @@ using System;
 using System.Threading;
 using System.Globalization;
 
+using NLog;
+
+using MyCSharp;
+
 namespace CentralProject
 {
 
@@ -14,14 +18,50 @@ namespace CentralProject
 		/// Main entry point
 		/// </summary>
 		/// <param name="args"></param>
+		[STAThread]
 		static void Main(string[] args)
 		{
+			var application = new MainApplication(args);
+			application.Run();
 		}
+
+		protected static Logger log = LogManager.GetCurrentClassLogger();
 
 		public MainApplication(string[] args)
 		{
-			Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("en-us");
-			Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("en-us");
+		}
+
+		public void Run()
+		{
+			RunSafe();
+		}
+
+		private void RunSafe()
+		{
+			try
+			{
+				RunInternal();
+			}
+			catch (Exception exception)
+			{
+				log.Error(exception.GetExceptionDescriptionAsText());
+				Assert.NotCritical(exception);
+			}
+		}
+
+		private void RunInternal()
+		{
+			log.Info("[>>>] Starting application...");
+			CurrentThreadCulture.SetEnglish();
+			RunPresentation();
+			log.Info("[XXX] Exiting application...");
+		}
+
+		/// <summary> Run Windows Presentation Foundation application </summary>
+		public void RunPresentation()
+		{
+			var presentation = new PresentationApplication();
+			presentation.Run();
 		}
 
 	}
---
+++ CentralProject/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+
+using NLog;
+
+namespace CentralProject
+{
+
+	public partial class MainWindow : Window
+	{
+
+		protected Logger log = LogManager.GetCurrentClassLogger();
+
+		public MainWindow()
+			: base()
+		{
+			log.Debug("Now creating...");
+			InitializeComponent();
+
+		}
+
+
+
+	}
+
+}
