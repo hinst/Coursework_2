8202b60a55af47e3117466385744b850127eb8c8
Alexander Savinoff <hinstance@yandex.ru>
2013-01-13 15:34:41 +0400

    Work in progress. Removed the .suo (user-specific) file.

---
+++ Backup-This.ps1
@@ -0,0 +1,6 @@
+﻿$env:Path = $env.Path + ';C:\Users\hinst\Docs\Pro\PowerShellScripts'
+. My7z.ps1
+$ScriptPath = $MyInvocation.MyCommand.Path
+$ScriptFolderPath = Split-Path $ScriptPath
+Write-Host $ScriptFolderPath
+Archive7z $ScriptFolderPath $VisualStudioProject
