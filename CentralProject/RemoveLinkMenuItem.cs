149eeb208bd31d8968992449050aa22e749e43c4
Alexander Savinoff <hinstance@yandex.ru>
2013-02-10 22:32:23 +0400

    RemoveLinkMenuItem.cs class created

---
+++ CentralProject/RemoveLinkMenuItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace Coursework_2
+{
+
+	public class RemoveLinkMenuItem : MenuItem
+	{
+
+		protected LinkControl link;
+
+		public RemoveLinkMenuItem Create(LinkControl link, NodeControl toNode)
+		{
+			Header = "Remove link to: " + toNode.Caption.Text;
+			this.link = link;
+			return this;
+		}
+
+		protected override void OnClick()
+		{
+			
+		}
+
+	}
+
+}
